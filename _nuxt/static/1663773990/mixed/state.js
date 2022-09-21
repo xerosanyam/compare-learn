2250d0451696f73d904f8275e18d36efceee9f80
xerosanyam <xerosanyam@users.noreply.github.com>
2022-09-21 15:26:33 +0000

    deploy: 5780a1af3fa1159436d726ca7175774d973100fb

---
+++ _nuxt/static/1663773990/mixed/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fcompare-learn\u002F_nuxt\u002Fstatic\u002F1663773990",layout:"default",error:a,serverRendered:true,routePath:"\u002Fmixed",config:{_app:{basePath:"\u002Fcompare-learn\u002F",assetsPath:"\u002Fcompare-learn\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"1f161aac"}}}}(null));
