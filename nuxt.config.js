dc459a770bcfa256997b28e91c7b11528c9c3ad6
Sanyam Jain <xerosanyam@gmail.com>
2024-04-20 00:07:38 +0530

    update description

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-        { hid: 'description', name: 'description', content: '' },
+        { hid: 'description', name: 'description', content: 'Learn by comparing. React vs Vue. Java vs Javascript, etc. Notes by @xerosanyam' },
       ],
       link: [
         {
