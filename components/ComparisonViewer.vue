a471bf7182ccd03352c9e142ec11c370b610745a
Sanyam Jain <xerosanyam@gmail.com>
2024-04-20 00:03:07 +0530

    use comparison viewer

---
+++ components/ComparisonViewer.vue
@@ -0,0 +1,62 @@
+<template>
+	<div class="max-w-full pb-64 mx-auto prose-sm prose">
+		<div class="flex">
+			<div class="max-w-40">
+				<Toc :pages="pages" />
+			</div>
+			<div class="w-5/6 p-2">
+				<div class="flex">
+					<div class="w-full text-2xl font-bold text-center">{{ topic1.topic }}</div>
+					<div class="w-full text-2xl font-bold text-center">{{ topic2.topic }}</div>
+				</div>
+				<div v-for="page in pages" :key="page" class="pb-8 border-b">
+					<div class="flex flex-row space-x-8 sm:space-x-32">
+						<ContentDoc v-if="data1[page]" class="flex-1 overflow-hidden" :path="data1[page]._path" />
+						<div v-else class="flex items-center justify-center flex-1 h-40">
+							<div>&nbsp;</div>
+						</div>
+						<ContentDoc v-if="data2[page]" :id="page" class="flex-1 overflow-hidden"
+							:path="data2[page]._path" />
+						<div v-else :id="page" class="flex items-center justify-center flex-1 h-40">
+							<div>&nbsp;</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+	topic1: Object,
+	topic2: Object
+})
+console.log('props:', props)
+
+let { data: data1 } = await useAsyncData('topic1', () => queryContent(props.topic1.path).find())
+console.log('data1:', data1)
+let { data: data2 } = await useAsyncData('topic2', () => queryContent(props.topic2.path).find())
+
+const allPages = [...data1.value, ...data2.value]
+
+const getSlug = (path) => {
+	return path?.split('/').pop()
+}
+const pages = []
+for (const page of allPages) {
+	if (!pages.includes(getSlug(page._path))) {
+		pages.push(getSlug(page._path))
+	}
+}
+
+data1 = data1.value.reduce((result, item) => {
+	result[getSlug(item._path)] = item
+	return result
+}, {})
+data2 = data2.value.reduce((result, item) => {
+	result[getSlug(item._path)] = item
+	return result
+}, {})
+</script>
--- pages/computer-science/paradigms/index.vue
+++ pages/computer-science/paradigms/index.vue
@@ -1,54 +1,5 @@
 <template>
-  <div>
-    <Toc :pages="pages" />
-    <div class="max-w-5xl px-4 pt-4 pb-64 mx-auto prose-sm prose">
-      <div class="flex">
-        <div class="w-full text-2xl font-bold text-center">Object</div>
-        <div class="w-full text-2xl font-bold text-center">Functional</div>
-      </div>
-      <div v-for="page in pages" :key="page" class="pb-8 border-b">
-        <div class="flex flex-row space-x-8 sm:space-x-32">
-          <ContentDoc v-if="data1[page]" :id="page" class="flex-1 overflow-hidden" :path="data1[page]._path" />
-          <div v-else :id="page" class="flex items-center justify-center flex-1 h-40">
-            <div>&nbsp;</div>
-          </div>
-          <ContentDoc v-if="data2[page]" class="flex-1 overflow-hidden" :path="data2[page]._path" />
-          <div v-else class="flex items-center justify-center flex-1 h-40">
-            <div>&nbsp;</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+  <ComparisonViewer :topic1="{ topic: 'Object', path: 'computer-science/paradigms/object' }"
+    :topic2="{ topic: 'Functional', path: 'computer-science/paradigms/functional' }"/>
 </template>
 
-
-<script setup>
-const url1 = 'computer-science/paradigms/object'
-const url2 = 'computer-science/paradigms/functional'
-
-let { data: data1 } = await useAsyncData('js', () => queryContent(url1).find())
-
-let { data: data2 } = await useAsyncData('js', () => queryContent(url2).find())
-
-const allPages = [...data1.value, ...data2.value]
-
-const getSlug = (path) => {
-  return path?.split('/').pop()
-}
-const pages = []
-for (const page of allPages) {
-  if (!pages.includes(getSlug(page._path))) {
-    pages.push(getSlug(page._path))
-  }
-}
-
-data1 = data1.value.reduce((result, item) => {
-  result[getSlug(item._path)] = item
-  return result
-}, {})
-data2 = data2.value.reduce((result, item) => {
-  result[getSlug(item._path)] = item
-  return result
-}, {})
-</script>
